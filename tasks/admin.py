e291d58e0287b678f9e196974d4b19e08c03da10
= <=>
2023-12-13 21:37:18 +0530

    validations

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'created_at', 'updated_at')
+
+admin.site.register(Task, TaskModelAdmin)
